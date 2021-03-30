a780f6ac311ff6727370d0522f15a358f09c1ce9
natalia906 <sns-91@web.de>
2021-03-30 14:24:43 +0200

    Goodbye
    Klasen Goodbye und GoodbyeTest erstellt

---
+++ src/main/java/maven/Goodbye.java
@@ -0,0 +1,13 @@
+package maven;
+
+public class Goodbye {
+
+	public Goodbye() {
+		System.out.println("Goodbye world!");
+	}
+	
+	public boolean isAlive() {
+		return true;
+	}
+
+}
---
+++ src/test/java/test/GoodbyeTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import maven.Goodbye;
+
+public class GoodbyeTest {
+	
+	@Test
+	public void test() {
+		Goodbye bye = new Goodbye();
+		assert(bye.isAlive());
+	}
+
+}
